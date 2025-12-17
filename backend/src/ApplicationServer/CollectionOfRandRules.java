438b8274554b2843045429f5d7f5cf0471ec3a22
mesuda sosei <bp23113@shibaura-it.ac.jp>
2025-12-17 15:40:40 +0900

    CollectionOfRandRulesクラス作成

---
+++ backend/src/ApplicationServer/CollectionOfRandRules.java
@@ -0,0 +1,24 @@
+package ApplicationServer;
+
+import java.util.ArrayList;
+
+public class CollectionOfRandRules {
+    public int ruleCount;
+    private ArrayList<RandRule> randRuleList;
+
+    public CollectionOfRandRules() {
+        this.randRuleList = Makerules();
+        this.ruleCount = randRuleList.size();
+    }
+    private ArrayList<RandRule>Makerules(){
+        ArrayList<RandRule>rulelist=new ArrayList<>();
+        RandRule rule1=new RandRule("rr01","a");
+        rulelist.add(rule1);
+        RandRule rule2=new RandRule("rr02","b");
+        rulelist.add(rule2);
+        return rulelist;
+    }
+    public ArrayList<RandRule> getRandRuleList(){
+        return this.randRuleList;
+    }
+}
