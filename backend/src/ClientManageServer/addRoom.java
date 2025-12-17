dfb2515a7b41bab06a59cfb4b3fa55616e2149a6
BP23055 <bp23055@shibaura-it.ac.jp>
2025-12-17 16:23:22 +0900

    addRoom ver1 (#6)

---
+++ backend/src/ClientManageServer/addRoom.java
@@ -0,0 +1,16 @@
+package ClientManageServer;
+
+    //新規ルームを作成し、リストに追加するメソッド
+
+    public void addRoom(int roomId, int numberOfPeople, int lives) {
+        // 新しいRoomインスタンスを生成
+        Room newRoom = new Room(roomId, numberOfPeople, lives);
+
+        // リストに追加
+        rooms.add(newRoom);
+
+        System.out.println("ルームID: " + roomId + " を作成しました。");
+    }
+
+
+
