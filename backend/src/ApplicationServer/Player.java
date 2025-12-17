2ae6cb93c02de1d635bb7ab56a0dc1fbd9e55411
BP23029 <bp23029@shibaura-it.ac.jp>
2025-12-17 16:06:33 +0900

    プレイヤーリスト関連のクラスを実装

---
+++ backend/src/ApplicationServer/Player.java
@@ -0,0 +1,13 @@
+package ApplicationServer;
+
+public class Player {
+    public String userid;
+    public int life;
+    public int selectednum;
+
+    public Player(String userid,int life,int selectednum){
+        this.userid=userid;
+        this.life=life;
+        this.selectednum=selectednum;
+    }
+}
