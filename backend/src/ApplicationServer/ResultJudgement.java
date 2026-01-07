ba9bd5230b745a360cea68207705ce078cd9ae04
mesuda sosei <bp23113@shibaura-it.ac.jp>
2026-01-07 17:10:42 +0900

    ResultJudgementクラス修正

--- backend/src/ApplicationServer/ResultJudgement.java
+++ backend/src/ApplicationServer/ResultJudgement.java
@@ -7,7 +7,7 @@ public class ResultJudgement {
     double result;
     double average;
     double ratio;
-    boolean manageLife(String ruleId,PlayerList pl){
+    void manageLife(String ruleId,PlayerList pl){
         switch (ruleId){
             case "RULE_ODD","RULE_EVEN","RULE_MULTIPLE_OF_3","RULE_PRIME"->{//通常通りライフの減算をするルールの場合
                 List<Player> winners=new ArrayList<Player>();//勝者を保存するインスタンス
@@ -31,7 +31,7 @@ public class ResultJudgement {
                     }
                 }
             }
-            case "RULE_CLOSESt_AND_FARTHEST"->{//一番近い人と遠い人のみライフ削減
+            case "RULE_CLOSEST_AND_FARTHEST"->{//一番近い人と遠い人のみライフ削減
                 List<Player> closest_losers=new ArrayList<Player>();//近い人を保存するインスタンス
                 List<Player> farthest_losers=new ArrayList<Player>();//遠い人を保存するインスタンス
                 double dif_min=100;//最小の差を保存する
@@ -64,7 +64,7 @@ public class ResultJudgement {
                     }
                 }
             }
-            case "RULE_LIFE_PLUS1_ON_JUST_AVERAGE"->{//ぴったりのときライフ+1
+            case "RULE_LIFE_PLUS1_ON_JUST_RESULT"->{//ぴったりのときライフ+1
                 List<Player> winners=new ArrayList<Player>();//勝者を保存するインスタンス
                 double dif_min=100;//最小の差を保存する
                 /*勝者を判定する*/
@@ -95,6 +95,77 @@ public class ResultJudgement {
                     }
                 }
             }
+            case "ONE_ON_ONE"->{//残っているプレイヤーが2人のとき
+                Player winner;//勝者を保存する
+                int flag_ratio;
+                /*残っているプレイヤーを取り出す*/
+                Player p1=pl.getPlayerList().get(0);
+                Player p2=pl.getPlayerList().get(1);
+                double dif_min=100;//計算結果との最小の差を保存
+                for(Player player:pl.getPlayerList()){
+                    if(player.selectednum!=-1){//ルール内の数字を選んだプレイヤー
+                        if(Math.abs(result-player.selectednum)<dif_min) {//計算結果との差が小さい場合
+                            winner = player;
+                            dif_min = Math.abs(result - player.selectednum);
+                        }
+                    }
+                }
+                if(this.ratio<1){//倍率が1未満の場合
+                    flag_ratio=0;
+                }else if(this.ratio==1){//倍率が1のとき
+                    flag_ratio=1;
+                }else{//倍率が1より大きい場合
+                    flag_ratio=2;
+                }
+                /*タイマン特別ルールの適応*/
+                switch(flag_ratio){
+                    case 0->{//倍率が1未満のとき
+                        /*0を選択したものがいた場合のみ100を選択したものが勝利*/
+                        if(p1.selectednum==100 && p2.selectednum==0){
+                            winner=p1;
+                        }else if(p1.selectednum==0 && p2.selectednum==100){
+                            winner=p2;
+                        }
+                    }
+                    case 1->{//倍率が1のとき
+                        /*数字が大きい方が勝利（ただし100を選択したものがいた場合のみ0を選択したものが勝者）*/
+                        int max=0;
+                        for(Player player:pl.getPlayerList()){
+                            if(max<player.selectednum){
+                                max=player.selectednum;
+                                winner=player;
+                            }
+                        }
+                        /*100を選択したものがいた場合のみ0を選択したものが勝者*/
+                        if(p1.selectednum==100 && p2.selectednum==0){
+                            winner=p2;
+                        }else if(p1.selectednum==0 && p2.selectednum==100){
+                            winner=p1;
+                        }
+                    }
+                    case 2->{//倍率が1より大きいとき
+                        /*100を選択したものがいた場合のみ0を選択したものが勝利*/
+                        if(p1.selectednum==100 && p2.selectednum==0){
+                            winner=p2;
+                        }else if(p1.selectednum==0 && p2.selectednum==100){
+                            winner=p1;
+                        }
+                    }
+                    default ->
+                        System.out.println("error of ratio");
+                }
+
+                for(Player player:pl.getPlayerList()){
+                    if(player.selectednum==-1){//ルール外の数字を選択した場合
+                        /*ライフ減算*/
+                        pl.SubLife(player,1);
+                    }else if(!player.equals(winner)){/*勝者でない場合*/
+                        /*ライフを0にする*/
+                        pl.SubLife(player,player.life);
+                    }
+                }
+
+            }
             default->{
                 List<Player> winners=new ArrayList<Player>();//勝者を保存するインスタンス
                 double dif_min=100;//最小の差を保存する
@@ -118,7 +189,6 @@ public class ResultJudgement {
                 }
             }
         }
-        return true;
     }
     boolean receiveVotingResult(String NumData){
         return true;
@@ -187,7 +257,7 @@ public class ResultJudgement {
                     }
                 }
                 break;
-            case "RULE_CLOSESt_AND_FARTHEST"://一番近い人と遠い人のみライフ削減
+            case "RULE_CLOSEST_AND_FARTHEST"://一番近い人と遠い人のみライフ削減
                 for(Player player:pl.getPlayerList()){
                     if(player.selectednum>=0 && player.selecteednum<=100){//数字が範囲内の場合
                         average+=player.selectednum;
@@ -198,7 +268,18 @@ public class ResultJudgement {
                     }
                 }
                 break;
-            case "RULE_LIFE_PLUS1_ON_JUST_AVERAGE"://ぴったりのときライフ+1
+            case "RULE_LIFE_PLUS1_ON_JUST_RESULT"://ぴったりのときライフ+1
+                for(Player player:pl.getPlayerList()){
+                    if(player.selectednum>=0 && player.selecteednum<=100){//数字が範囲内の場合
+                        average+=player.selectednum;
+                    }else{//数字が範囲外の場合
+                        /*ルールに適しない数字は計算から削除*/
+                        player.selectednum=-1;
+                        playerCount--;
+                    }
+                }
+                break;
+            case "ONE_ON_ONE"://プレイヤーが2人になった場合
                 for(Player player:pl.getPlayerList()){
                     if(player.selectednum>=0 && player.selecteednum<=100){//数字が範囲内の場合
                         average+=player.selectednum;
@@ -228,7 +309,9 @@ public class ResultJudgement {
         /*倍率を算出*/
         this.ratio=(double)(randomValue+8)/10;
         /*平均計算*/
-        average=average/playerCount;
+        if(playerCount!=0){
+            average=average/playerCount;
+        }
         /*計算結果を返す*/
         this.result=this.average*this.ratio;
         return this.result;
