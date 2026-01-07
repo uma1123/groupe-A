76be0f59944d2c4725f86f841ae70f0da03c9a8a
mesuda sosei <bp23113@shibaura-it.ac.jp>
2026-01-07 17:11:19 +0900

    CollectionOfRandRules修正

--- backend/src/ApplicationServer/CollectionOfRandRules.java
+++ backend/src/ApplicationServer/CollectionOfRandRules.java
@@ -12,10 +12,20 @@ public class CollectionOfRandRules {
     }
     private ArrayList<RandRule>Makerules(){
         ArrayList<RandRule>rulelist=new ArrayList<>();
-        RandRule rule1=new RandRule("rr01","a");
+        RandRule rule1=new RandRule("RULE_ODD","奇数のみ");
         rulelist.add(rule1);
-        RandRule rule2=new RandRule("rr02","b");
+        RandRule rule2=new RandRule("RULE_EVEN","偶数のみ");
         rulelist.add(rule2);
+        RandRule rule3=new RandRule("RULE_MULTIPLE_OF_3","3の倍数のみ");
+        rulelist.add(rule3);
+        RandRule rule4=new RandRule("RULE_PRIME","素数のみ");
+        rulelist.add(rule4);
+        RandRule rule5 =new RandRule("RULE_CLOSEST_AND_FARTHEST","一番近い人と遠い人のみライフ削減");
+        rulelist.add(rule5);
+        RandRule rule6 =new RandRule("RULE_LIFE_PLUS1_ON_JUST_RESULT","ぴったりのときライフ+1");
+        rulelist.add(rule6);
+
+
         return rulelist;
     }
     public ArrayList<RandRule> getRandRuleList(){
