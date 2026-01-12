39e58b4a1c545ff2eda68ffdf5e55916d5189cd2
BP23055 <bp23055@shibaura-it.ac.jp>
2026-01-12 13:50:18 +0900

    test1 (#40)

--- backend/src/ClientManageServer/TEST/RoomServiceTest.java
+++ backend/src/ClientManageServer/TEST/RoomServiceTest.java
@@ -1,5 +1,7 @@
-package ClientManageServer;
+package ClientManageServer.TEST;
 
+import ClientManageServer.Room;
+import ClientManageServer.roomService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
