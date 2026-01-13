b12f5503f5c947569ea65c9f968ec11c49f8bce3
bp23016 <bp23016@shibaura-it.ac.jp>
2026-01-13 15:26:21 +0900

    roomList ver1 (#43)
    ルームサービスクラス修正1
    ルームコントローラ削除
    ジョインリクエスト削除
    ルームサービステスト
    ルームテスト
    メッセージクラスに要素を追加

--- backend/src/ClientManageServer/Message.java
+++ backend/src/ClientManageServer/Message.java
@@ -1,13 +1,22 @@
 package ClientManageServer;
 
 public class Message {
-    int id;
+    private String action;   // 呼び出すメソッドをここで判断する（ex:ルーム参加だったらJOIN）
+     String id;
     String password;
     String message;
+    int roomId;
+    int numOfPlayer;
+    int numOfLife;
 
-    public Message(int id, String password, String message) {
+    public Message(String id, String password, String message) {
         this.id =id;
         this.password = password;
         this.message = message;
     }
+    public String getAction() { return action; }
+    public int getRoomId() { return roomId; }
+    public String getUserId() { return id; }
+    public int getMaxPlayers() { return numOfPlayer; }
+    public int getLife() { return numOfLife; }
 }
