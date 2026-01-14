012481a6da611dc4e1db2aa34c4bca2aafbae740
mesuda sosei <bp23113@shibaura-it.ac.jp>
2026-01-14 19:06:05 +0900

    RuleLotteryの修正

--- backend/src/ApplicationServer/RuleLottery.java
+++ backend/src/ApplicationServer/RuleLottery.java
@@ -9,15 +9,25 @@ public class RuleLottery {
         this.currentRule=new Rule();
     }
     public String drawRandRule(int playerCount){//プレイヤーが2人のときは専用のルール
+        /*ランダムルールリストを持ってくる*/
+        ArrayList<RandRule> randRules = currentRule.ruleList.getRandRuleList();
+        RandRule selectedRule=null;
         if(playerCount==2){
+            for(RandRule rr:randRules){
+                if(rr.getRandRuleId().equals("ONE_ON_ONE")){
+                    selectedRule=rr;
+                    currentRule.setCurrentRuleId(selectedRule.getRandRuleId());
+                }
+            }
             return "ONE_ON_ONE";
         }
-        /*ランダムルールの数のうちから乱数で数字を出力*/
-        int index=new Random().nextInt(currentRule.ruleList.getRuleCount());
-        /*ランダムルールリストを持ってくる*/
-        ArrayList<RandRule>randRules=currentRule.ruleList.getRandRuleList();
-        /*ランダムルールリストからランダムに選ばれた数字番目のルールを代入*/
-        RandRule selectedRule=randRules.get(index);
+        /*ONE_ON_ONE以外を引くまで繰り返し*/
+        do {
+            /*ランダムルールの数のうちから乱数で数字を出力*/
+            int index = new Random().nextInt(currentRule.ruleList.getRuleCount());
+            /*ランダムルールリストからランダムに選ばれた数字番目のルールを代入*/
+            selectedRule = randRules.get(index);
+        }while(selectedRule.getRandRuleId().equals("ONE_ON_ONE"));
         /*抽選されたランダムルールのIdを保存*/
         currentRule.setCurrentRuleId(selectedRule.getRandRuleId());
         /*抽選されたランダムルールのIdを返す*/
