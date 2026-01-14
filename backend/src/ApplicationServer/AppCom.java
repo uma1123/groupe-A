d0a7b7813c20f55239f18daf6fdf3a058cef59f6
BP23029 <bp23029@shibaura-it.ac.jp>
2026-01-14 15:12:20 +0900

    仕様変更によりエンドポイントが一つでよくなったため、他のエンドポイントを削除

--- backend/src/ApplicationServer/AppCom.java
+++ backend/src/ApplicationServer/AppCom.java
@@ -9,40 +9,15 @@ public class AppCom {
 
 
     public static void main(String[] args) throws Exception {
-        //数字受け取りエンドポイント
-        Server numserver = new Server(protocol, port, contextRoot, num, AppCom.class/*, EndpointExample.class*/);
-        System.out.println("numserver: " + numserver);
-        //プレイヤー情報受信エンドポイント
-        Server playerinfoserver = new Server(protocol, port, contextRoot, playerinfo, AppCom.class/*, EndpointExample.class*/);
-        System.out.println("playerinfoserver: " + playerinfoserver);
-        //ゲーム開始受信エンドポイント
-        Server startserver = new Server(protocol, port, contextRoot, start, AppCom.class/*, EndpointExample.class*/);
-        System.out.println("startserver: " + startserver);
-        //ルール情報受信エンドポイント
-        Server ruleserver = new Server(protocol, port, contextRoot, rule, AppCom.class/*, EndpointExample.class*/);
-        System.out.println("ruleserver: " + ruleserver);
-        //勝敗送信エンドポイント
-        Server resultserver = new Server(protocol, port, contextRoot, result, AppCom.class/*, EndpointExample.class*/);
-        System.out.println("resultserver: " + resultserver);
-        //結果送信エンドポイント
-        Server judgeserver = new Server(protocol, port, contextRoot, rule, AppCom.class/*, EndpointExample.class*/);
-        System.out.println("judgeserver: " + judgeserver);
+        Server server = new Server(protocol, port, contextRoot, null, AppCom.class/*, EndpointExample.class*/);
+        System.out.println("numserver: " + server);
+
 
         try {
-            numserver.start();
-            playerinfoserver.start();
-            startserver.start();
-            ruleserver.start();
-            resultserver.start();
-            judgeserver.start();
+            server.start();
             System.in.read();
         } finally {
-            numserver.stop();
-            playerinfoserver.stop();
-            startserver.stop();
-            ruleserver.stop();
-            resultserver.stop();
-            judgeserver.stop();
+            server.stop();
         }
     }
 
